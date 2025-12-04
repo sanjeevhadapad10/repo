d343d05f8fc7f4d02e76f4eef540061bedfa2166
sanjeevhadapad10 <sanjeevhadapad10@gmail.com>
2025-12-04 14:26:12 +0530

    Update text_addition.py

--- text_addition.py
+++ text_addition.py
@@ -1,1 +1,9 @@
+from addition import add
+def test_add_positive_numbers():
+  assert add(2,3) ==5
 
+def test_add_negative_nmubers():
+  assert add(-4,-6) == -10
+
+def test_add_zero():
+  assert add(0,5) == 5
